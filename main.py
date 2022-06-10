e68d9512d3afbe03d49e1e565ae70cbce5da19b0
user2 <anna.iarajuli@gmail.com>
2022-06-10 16:28:29 +0400

    registration and authorization function

--- main.py
+++ main.py
@@ -1,16 +1,30 @@
+import re
+
 from flask import Flask, redirect, url_for, render_template, request, session, flash
 from flask_sqlalchemy import SQLAlchemy
-from admin import admin_page
-from sqlalchemy.sql import func
-
+from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import InputRequired, Length, ValidationError
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'Lecture14'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///books.sqlite'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.register_blueprint(admin_page, url_prefix='/admin')
-
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+
+app.config['SECRET_KEY'] = 'brutuseti'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///books.sqlite'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.sqlite'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = "login"
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 class Books(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -43,6 +57,40 @@ class Books(db.Model):
 # db.session.add(b1)
 # db.session.commit()
 
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), nullable=False, unique=True)
+    password = db.Column(db.String(80), nullable=False)
+
+# db.create_all()
+
+
+class RegisterForm(FlaskForm):
+    username = StringField(validators=[
+                           InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Username"})
+
+    password = PasswordField(validators=[
+                             InputRequired(), Length(min=8, max=20)], render_kw={"placeholder": "Password"})
+
+    submit = SubmitField('Register')
+
+    def validate_username(self, username):
+        existing_user_username = User.query.filter_by(
+            username=username.data).first()
+        if existing_user_username:
+            raise ValidationError(
+                'This username already exists. Please choose a different one.')
+
+
+
+class LoginForm(FlaskForm):
+    username = StringField(validators=[
+                           InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Username"})
+
+    password = PasswordField(validators=[
+                             InputRequired(), Length(min=8, max=20)], render_kw={"placeholder": "Password"})
+
+    submit = SubmitField('Log in')
 
 
 
@@ -52,34 +100,75 @@ def home():
     return render_template('index.html')
 
 
+@app.route('/register', methods=['POST', 'GET'])
+def register():
+    # if request.method == 'POST':
+    #     email = request.form['email']
+    #     password = request.form['password']
+    #     username = request.form['username']
+    #     session['username'] = username
+    #     regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
+    #     if email == '' or password == '' or username == '':
+    #         flash('Please fill in all the required fields', 'error')
+    #     elif (re.fullmatch(regex, email)) == False:
+    #         flash('Enter valid email', 'error')
+    #     elif len(password) < 8:
+    #         flash('Password must be 8 or more characters long', 'error')
+    #     else:
+    #         return redirect(url_for('home'))
+
+    form = RegisterForm()
+
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data)
+        new_user = User(username=form.username.data, password=hashed_password)
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect(url_for('login'))
+
+    return render_template('register.html', form=form)
+
 @app.route('/login', methods=['POST', 'GET'])
 def login():
-    if request.method == 'POST':
-        username = request.form['username']
-        session['username'] = username
-        return redirect(url_for('user'))
+    # if request.method == 'POST':
+    #     username = request.form['username']
+    #     password = request.form['username']
+    #     if username == register(username) and password == register(password):
+    #         return render_template('login.html')
+    #     else:
+    #         flash('Username or password mismatch', 'error')
 
-    return render_template('login.html')
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        #ამოწმებს db-ში არის თუ არა მომხმარებელი და თუ არის შემდეგ ამოწმებს
+        #შეყვანილ პაროლს. თუ დაემთხვევა პაროლიც მაშინ შეუშვებს მომხმარებელს თავის პროფილზე
+        user = User.query.filter_by(username=form.username.data).first()
+        if user:
+            if bcrypt.check_password_hash(user.password, form.password.data):
+                login_user(user)
+                return redirect(url_for('user'))
+
+    return render_template('login.html', form=form)
 
 
 
 @app.route('/user')
+@login_required
 def user():
     subjects = ['Python', 'Calculus', 'DB']
     return render_template('user.html',  subjects=subjects)
 
 
-@app.route('/<name>/<age>')
-def userage(name, age):
-    return f'Hello {name}, your age is {age}'
-
 @app.route('/logout')
+@login_required
 def logout():
-    session.pop('username', None)
-    return 'you are logged out'
+    logout_user()
+    return redirect(url_for('register'))
 
 
 @app.route('/books', methods=['GET', 'POST'])
+@login_required
 def books():
     if request.method == 'POST':
 
